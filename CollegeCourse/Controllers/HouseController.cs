581e7ffbfc80f7a94aabc525beb67b98f207d011
Aftab Ahmed <aftab@techsolutions.net>
2017-10-20 16:22:31 +0500

    College Course

---
+++ CollegeCourse/Controllers/HouseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeCourse.Controllers
+{
+    public class HouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
